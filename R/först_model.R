c081a04a261cfeb33e8ad4d058d4b1d257093347
Tim Tito Rademacher <ttr22@cam.ac.uk>
2017-11-22 22:46:52 -0700

    Introduce plotting and started to debug the translation

--- R/först_model.R
+++ R/först_model.R
@@ -9,8 +9,19 @@ rm (list = ls ())
 # Load necessary dependencies
 #----------------------------------------------------------------------------!
 source ('R/readData.R')
+library ('PopED')
 
-water_supplies   <- Inf      #
+# Get the initial observations
+#----------------------------------------------------------------------------!
+dataSurv <- readDataSurv ()
+dataTime <- readDataTime ()
+dataDead <- readDataDead ()
+
+# Boolean: T = comparison to empirical data, F = long term simulation
+validationNOTlongterm <- T
+
+# Initialise variables
+water_supplies   <- Inf     #
 harv_parameters <- c (0, 1) # 
 nspecies <- 2               # Beech is species = 1, spruce is species = 2
 
@@ -24,8 +35,18 @@ sizes2      <- sizes^2                # square of stem sizes
 sizes3      <- sizes^3                # cube of stem sizes
 sizeclasses <- length (sizes)         # number of different stem sizes considered
 dt          <- 0.01                   # time steps per year
-years       <- 1000                   # number of years in simulation
+
+# Determine the length of the simulation
+if (validationNOTlongterm) {
+  checkpoints <- dataTime - dataTime [1]      # points in simulation time at which empirical data is available
+  checkpoints <- checkpoints [-4]             # ignore this one to allow 3x3 plot
+  years <- checkpoints [length (checkpoints)] # number of years simulated
+} else {
+  checkpoints <- c (dataTime [length (dataTime)] - dataTime [1], 125, 200, 300, 500, 1000) #last point for which empirical data is available, and several years after that
+  years <- checkpoints [length (checkpoints)]  # number of years simulated
+}
 timesteps   <- ceiling (years / dt)   # number of time steps in simulation
+timesteps   <- 7 / dt
 
 # Set initial values to zero
 #----------------------------------------------------------------------------!
@@ -37,6 +58,7 @@ DiameterGrowth <- rep (0, sizeclasses)
 DeathRate      <- rep (0, sizeclasses)
 u              <- matrix (rep (0, nspecies * sizeclasses), nrow = sizeclasses)
 u1             <- rep (0, sizeclasses)
+v              <- matrix (rep (0, nspecies * sizeclasses), nrow = sizeclasses)
 
 # Species parameters for 
 #     - beech  (first  entry)
@@ -60,21 +82,56 @@ HarvestRate  <- 0.0
 
 # set initial size distribution (u) according to first observation
 #----------------------------------------------------------------------------!
-dataSurv <- readDataSurv ()
-dataDead <- readDataDead ()
 for (species in 1:nspecies) {
   # empirical diameter data is in mm, convert to meters
   u0 <- 0.001 * c (dataSurv [[species]] [[1]] [, 1], dataDead [[species]] [[1]] [, 1])
-  u [1, species] <- sum (u0 <= sizes [1])
+  u [1, species] <- sum (u0 <= sizes [1], na.rm = T)
   for (i in 2:sizeclasses) {
     k <- (u0 > sizes [i - 1]) & (u0 <= sizes [i])
-    u [i, species] <- round (sum (k) / dx) # convert number of individuals with diameter between sizes(i-1) and sizes (i) to density
+    u [i, species] <- round (sum (k, na.rm = T) / dx) # convert number of individuals with diameter between sizes (i-1) and sizes (i) to density
   }
 }
+
+# function for ploting size distribution
+#----------------------------------------------------------------------------!
+pl.sizeDistribution <- function (u, v, sizes, y_max = 4000, title) {
+  plot (x     = sizes,
+        y     = u [, 1],
+        xlab  = 'dbh [m]',
+        ylab  = 'number of trees',
+        xlim  = c (0, 1.5),
+        ylim  = c (0, y_max),
+        typ   = 'l',
+        col   = 'blue',
+        lty   = 2,
+        las   = 1,
+        main = title)
+  lines (x    = sizes,
+         y    = u [, 2],
+         col  = 'red',
+         lty  = 2)
+  lines (x    = sizes,
+         y    = v [, 1],
+         col  = 'blue')
+  lines (x    = sizes,
+         y    = v [, 2],
+         col  = 'red')
+}
+y_maxs <- c (4000, 2000, 1000, 600, 600, 600, 400, 400, 300)
+
+# Plot initial size distribution
+layout (matrix (1:9, nrow = 3, byrow = T))
+par (mar = c (5, 5, 3, 1))
+pl.sizeDistribution (u = u, v = u, sizes = sizes, y_max = 4000, title = '0 years')
 
 # Set timer to measure execution time
 #----------------------------------------------------------------------------!
 start.time <- Sys.time ()
+
+# Function to calculate incident light upon the trees of sizeclass i
+#calcLayer <- function (species, i, sizes2 = sizes2) {
+#  Ind_Light [i] <- Ind_Light [i + 1] + u [i + 1, species] * sizes2 [i + 1]
+#}
 
 # Loop over timesteps
 #----------------------------------------------------------------------------!
@@ -101,7 +158,6 @@ for (t in 1:timesteps) {
   }
 
   for (species in 1:nspecies) {
-
     # calculate the different components determining the size distribution
     # one time step later. this is done as described in the paper
 
@@ -112,10 +168,11 @@ for (t in 1:timesteps) {
 
     # Death
     MassGrowth <- DiameterGrowth * sizes2
-    DeathRate  <- 0.5 * DeathPar [species] / max (0, MassGrowth)
+    DeathRate  <- 0.5 * DeathPar [species] / pmax (0, MassGrowth)
     DeathRate [is.nan (DeathRate)] <- 0
 
     end <- sizeclasses
+    # The error seemed to have been downstream of this
     GrowthFlux                 <- DiameterGrowth * u [, species]
     GrowthGradient [1]         <- (GrowthFlux [2]     - GrowthFlux [1])         / dx
     GrowthGradient [end]       <- (GrowthFlux [end]   - GrowthFlux [end-1])     / dx
@@ -127,6 +184,25 @@ for (t in 1:timesteps) {
     u1 [1] <- ReproductionRate [species] * TotalLeafArea [species] # birth of new trees
     u [, species] <- u1
   }
+
+  # Plot the size distribution
+  k0 <- which (t * dt == checkpoints) # checkpoints [k0] = t*dt, if there is such a k0
+  if (!isempty (k0)) {
+    if (validationNOTlongterm) {
+      for (species in 1:nspecies) {
+        # empirical diameter data is in mm, convert to meters
+        v0 <- 0.001 * c (dataSurv [[species]] [[1]] [, k0], dataDead [[species]] [[1]] [, k0])
+        v [1, species] <- round (sum (v0 <= sizes [1], na.rm = T) / dx)
+        for (i in 2:sizeclasses) {
+          k <- (v0 > sizes [i - 1]) & (v0 <= sizes [i])
+          v [i, species] <- round (sum (k, na.rm = T) / dx) # convert number of individuals with diameter between sizes (i-1) and sizes (i) to density
+        }
+      }
+      print (c (u, v))
+      pl.sizeDistribution (u = u, v = v, sizes = sizes, y_max = y_maxs [k0],
+                           title = paste (as.character (checkpoints [k0]), ' years', sep = ''))     
+    }
+  } # Still need to introduce plotting functionality for long-term simulations
 }
 
 # Determine time it took to run
--- R/readData.R
+++ R/readData.R
@@ -6,13 +6,20 @@ library (R.matlab)
 
 readDataSurv <- function (warning = 0) {
   data <- readMat ('matlab/all_sites_data_mixed.mat')
-  dataSurv <<- data [["data.surv"]]
+  dataSurv <- data [["data.surv"]]
+  time <- data [['time']]
   return (dataSurv)
+}
+
+readDataTime <- function (warning = 0) {
+  data <- readMat ('matlab/all_sites_data_mixed.mat')
+  dataTime <- data [["time"]]
+  return (dataTime)
 }
 
 readDataDead <- function (warning = 0) {
   data <- readMat ('matlab/all_sites_data_mixed.mat')
-  dataDead <<- data [["data.dead"]]
+  dataDead <- data [["data.dead"]]
   return (dataDead)
 }
 
